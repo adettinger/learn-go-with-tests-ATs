9e4676779cd13aa1547701125ce3570bd8f6c76f
adettinger <apddeapp@gmail.com>
2026-01-08 13:03:39 +0100

    WIP grpc

--- cmd/httpserver/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Starting container..."
-./svr || echo "Server crashed with exit code $?"
-echo "Server exited, keeping container alive for debugging"
-# Keep container running for debugging
-tail -f /dev/null
