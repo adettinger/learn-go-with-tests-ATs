5cb4aa843485ca03cdebd0f13a499cd6d3833ffe
adettinger <apddeapp@gmail.com>
2026-01-08 16:05:49 +0100

    default greet to World if name is empty

--- domain/interactions/greet_test.go
+++ domain/interactions/greet_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/adettinger/learn-go-with-tests-ATs/domain/interactions"
 	"github.com/adettinger/learn-go-with-tests-ATs/specifications"
+	"github.com/alecthomas/assert/v2"
 )
 
 func TestGreet(t *testing.T) {
@@ -12,4 +13,8 @@ func TestGreet(t *testing.T) {
 		t,
 		specifications.GreetAdapter(interactions.Greet),
 	)
+
+	t.Run("default name to world", func(t *testing.T) {
+		assert.Equal(t, "Hello, World", interactions.Greet(""))
+	})
 }
